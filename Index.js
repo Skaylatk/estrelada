ebac07151df9fe2edf0563ae6729942d6db21648
alef1mil <153133594+alef1mil@users.noreply.github.com>
2024-10-05 06:13:49 -0300

    Atualizar o Index.js

--- Index.js
+++ Index.js
@@ -3,21 +3,28 @@ const path = require('path');
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'public' ,'htmls', 'index.html'));
 });
 
 app.post('/submit', (req, res) => {
-  const { username, idade, email, senha} = req.body;
-  console.log(username, idade, email, senha)
-  
-})
+  const { username, senha} = req.body;
+  console.log(username, senha)
+  res.send('<h1> O SITE AINDA NAO ESTA PRONTO!')
+
+});
+
+app.use((req, res) => {
+  res.status(404).send('<h1>Essa página não existe</h1>');
+});
 
 app.listen(3000, () => {
   console.log('⏰ INICIANDO SITE.')
   setTimeout(function() {
     console.log('✅ SITE INICIADO.')
   }, 3000)
-  
+
 })
